c2fe1b6cafb702aba3ba4f87e173ed7e73e62c55
vinoj-m <vinojmurugapandiyan@gmail.com>
2026-02-22 21:10:42 +0530

    My twosum problem

---
+++ src/TwoPoiniters/Twosum.java
@@ -0,0 +1,30 @@
+package TwoPoiniters;
+
+public class Twosum {
+
+        public int[] twoSum(int[] numbers, int target) {
+
+            int l = 0 ;
+            int r = numbers.length-1 ;
+
+            while ( l < r)
+            {
+                int sum = numbers[l] + numbers[r] ;
+                if (sum > target){
+                    r-- ;
+
+                }
+                else if ( sum < target){
+                    l++ ;
+
+                }
+                else if (sum == target ){
+
+                    return new int [] { l+1 , r+1};
+                }
+            }
+            return new int[0] ;
+
+        }
+    }
+
