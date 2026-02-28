2c32437d04eaaf0044f070084d54c9e4d5135824
vinoj-m <vinojmurugapandiyan@gmail.com>
2026-02-28 12:04:22 +0530

    My remove Duplicate in sorted array problem

---
+++ src/TwoPoiniters/removeDuplicate.java
@@ -0,0 +1,20 @@
+package TwoPoiniters;
+
+public class removeDuplicate {
+    public static void main(String[] args) {
+        int [] arr = {1,1,2};
+        System.out.println(removeDuplicates(arr));
+    }
+    public static int removeDuplicates(int[] nums) {
+
+        int i =0;
+        for (int j =1 ; j<nums.length ; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+
+    }
+}
