e849c6960bf7f91cc23e1cbbdbb772eb433d63e2
vinoj-m <vinojmurugapandiyan@gmail.com>
2026-04-08 11:47:39 +0530

    string

---
+++ src/Strings/main.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class main {
+    public static void main(String[] args) {
+        String a = new String("vinoj");
+        String b = new String("vinoj");
+        System.out.println(a==b);
+    }
+}
